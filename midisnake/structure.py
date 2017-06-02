95ce3e45e32182570a6194d20a434c4638fd6da9
Ennis Massey <ennisbaradine@gmail.com>
2017-06-02 14:09:23 +1200

    Remove useless imports

--- midisnake/structure.py
+++ midisnake/structure.py
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-from typing import AnyStr, List, Dict, Any, cast
+from typing import List
 from io import BufferedReader
 from abc import ABCMeta, abstractmethod
 
